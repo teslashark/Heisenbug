81835af340f9502bc288adaaf98fec4b45c58583
Janet Davis <davisj@whitman.edu>
2017-01-26 20:32:30 -0800

    Initial commit

---
+++ src/scaleplayer/ScalePlayer.java
@@ -0,0 +1,52 @@
+/*
+ * CS 300-A, 2017S
+ */
+package scaleplayer;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * This JavaFX app lets the user play scales.
+ * @author Janet Davis 
+ * @author Put your name here!
+ * @author Put your partner's name here!
+ * @since January 26, 2017
+ */
+public class ScalePlayer extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
