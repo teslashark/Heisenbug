cc1690f9724fc9f49c154e57c795307ec9f769be
Janet Davis <davisj@whitman.edu>
2017-01-29 19:14:37 -0800

    Buttons and menu present and correctly positioned

--- src/scaleplayer/ScalePlayer.java
+++ src/scaleplayer/ScalePlayer.java
@@ -6,38 +6,72 @@ package scaleplayer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  * This JavaFX app lets the user play scales.
  * @author Janet Davis 
- * @author Put your name here!
- * @author Put your partner's name here!
+ * @author AOLUTION - PROJECT 1
  * @since January 26, 2017
  */
 public class ScalePlayer extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+
+        Button startButton = new Button();
+        startButton.setText("Play scale");
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Start");
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Button stopButton = new Button();
+        stopButton.setText("Stop playing");
+        stopButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Stop");
+            }
+        });
+        
+        HBox hbox = new HBox(8);
+        hbox.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.getChildren().addAll(startButton, stopButton);
+        
+        
+        Menu menuFile = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        });        
+        menuFile.getItems().addAll(exit);
+        
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(menuFile);
+
+        BorderPane root = new BorderPane();
+        root.setTop(menuBar);
+        root.setCenter(hbox);
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Scale Player");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
